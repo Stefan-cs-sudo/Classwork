0ae69c9c035fa0db87fba0de533c6e68ce3699cc
CretaciousStrategist <stefi.deac25@gmail.com>
2026-05-11 09:01:10 +0300

    NUMERE

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,33 @@
 package org.example;
 
+import java.util.Vector;
+
+import static java.lang.IO.print;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+    public static Vector<Integer> getNumbers()
+    {
+
+        Vector<Integer> v = new Vector<>();
+        for(int i=0;i<100;i++)
+        {
+            v.add(i);
+        }
+
+   return v;
+    }
+
+    static void main() {
+
+
+
+
+      Vector<Integer> v = new Vector<>();
+      v=getNumbers();
+      print(v);
         }
     }
-}
+
